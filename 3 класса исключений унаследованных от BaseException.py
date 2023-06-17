27d97771db86c6eb00fc2051c7f680951c8f02e6
G0gss <123230512+G0gss@users.noreply.github.com>
2023-06-17 09:38:08 +0300

    3 класса исключений унаследованных от BaseException.py

--- 3 класса исключений унаследованных от BaseException.py
+++ 3 класса исключений унаследованных от BaseException.py
@@ -1,5 +1,5 @@
-class MyException1(Exception): # Определяем три класса исключений: MyException1, MyException2 и MyException3. 
-    def init(self, message): # Каждый класс наследуется от базового класса Exception, и определяет метод init для задания сообщения об ошибке. 
+class MyException1(Exception): 
+    def init(self, message):
         self.message = message
         
 class MyException2(Exception):
@@ -10,21 +10,18 @@ class MyException3(Exception):
     def init(self, message):
         self.message = message
         
-def test_function(num): #Определяем функцию test_function с одним аргументом num, которая проверяет значение num
+def test_function(num): 
     if num < 0:
         raise MyException1('Число должно быть положительным')
     elif num > 100:
         raise MyException2('Число должно быть меньше или равно 100')
     else:
         raise MyException3('Произошла неизвестная ошибка')
-    # Если num меньше нуля, то возбуждается исключение MyException1 с сообщением "Число должно быть положительным".
-    # Если num больше 100, то возбуждается исключение MyException2 с сообщением "Число должно быть меньше или равно 100".
-    # В остальных случаях возбуждается исключение MyException3 с сообщением "Произошла неизвестная ошибка".
 try:
-    test_function(150) # В блоке try-except вызываем функцию test_function с аргументом 150. 
-except MyException1 as e: #Если в функции возникает исключение MyException1, то перехватываем его и выводим сообщение об ошибке.
+    test_function(150) 
+except MyException1 as e:
     print(e.message)
-except MyException2 as e: #Если возникает исключение MyException2, то выводим сообщение об ошибке этого типа исключения
+except MyException2 as e: 
     print(e.message)
-except MyException3 as e: #Если возникает исключение MyException3, то также выводим сообщение об ошибке этого типа исключения.
+except MyException3 as e: 
     print(e.message)
